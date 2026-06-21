fae6f8a4380d41ae780898a39f84ee6ac308fde8
Gabe Eslinger <38473185+Gabe284@users.noreply.github.com>
2026-06-21 03:43:52 -0500

    Inherit AOSP base for recovery root staging

--- twrp_kansas.mk
+++ twrp_kansas.mk
@@ -1,5 +1,14 @@
+# Architecture configuration must precede the base product inheritance.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Populate the standard Android root filesystem required while constructing
+# the standalone recovery ramdisk fragment in vendor_boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit common TWRP configuration.
 $(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit Kansas device configuration.
 $(call inherit-product, device/motorola/kansas/device.mk)
 
 PRODUCT_DEVICE := kansas
